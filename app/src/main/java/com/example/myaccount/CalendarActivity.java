0172c5ce21c828301a069849d00a648a4a846145
ou gunagye <3250679635@qq.com>
2021-11-09 14:25:49 +0800

    完善一下日历的样式

--- app/src/main/java/com/example/myaccount/CalendarActivity.java
+++ app/src/main/java/com/example/myaccount/CalendarActivity.java
@@ -4,11 +4,19 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import io.blackbox_vision.materialcalendarview.view.CalendarView;
+
+
 public class CalendarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendar_view);
+        //calendarView.setWeekDayLabels(new String[]{"日", "一", "二", "三", "四", "五", "六"});
+
     }
+
+
 }
