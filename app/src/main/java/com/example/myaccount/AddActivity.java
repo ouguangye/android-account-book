79152ca4417d9f375d9d811ba3e705105df4ae7f
ouguangye <3250679635@qq.com>
2021-11-17 22:33:43 +0800

    修复部分警告

--- app/src/main/java/com/example/myaccount/AddActivity.java
+++ app/src/main/java/com/example/myaccount/AddActivity.java
@@ -1,36 +1,34 @@
 package com.example.myaccount;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import android.graphics.Typeface;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class AddActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private ViewPager2 viewPager2;
 
-    private int activeColor = Color.parseColor("#ffffff");
-    private int normalColor = Color.parseColor("#ffffff");
+    private final int activeColor = Color.parseColor("#ffffff");
+    private final int normalColor = Color.parseColor("#ffffff");
 
-    private int activeSize = 17;
-    private int normalSize = 17;
+    private final int activeSize = 17;
+    private final int normalSize = 17;
 
     private ArrayList<Fragment> fragments;
     private TabLayoutMediator mediator;
@@ -41,13 +39,9 @@ public class AddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add);
 
         ImageView back_button = findViewById(R.id.back_to_main);
-        back_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AddActivity.this,MainActivity.class);
-                startActivity(intent);
-            };
-
+        back_button.setOnClickListener(view -> {
+            Intent intent = new Intent(AddActivity.this,MainActivity.class);
+            startActivity(intent);
         });
 
         tabLayout = findViewById(R.id.tab_layout);
@@ -75,41 +69,41 @@ public class AddActivity extends AppCompatActivity {
         //viewPager 页面切换监听监听
         viewPager2.registerOnPageChangeCallback(changeCallback);
 
-        mediator = new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
-            @Override
-            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                //这里可以自定义TabView
-                TextView tabView = new TextView(AddActivity.this);
+        mediator = new TabLayoutMediator(tabLayout, viewPager2, (tab, position) -> {
+            //这里可以自定义TabView
+            TextView tabView = new TextView(AddActivity.this);
 
-                int[][] states = new int[2][];
-                states[0] = new int[]{android.R.attr.state_selected};
-                states[1] = new int[]{};
+            int[][] states = new int[2][];
+            states[0] = new int[]{android.R.attr.state_selected};
+            states[1] = new int[]{};
 
-                int[] colors = new int[]{activeColor, normalColor};
-                ColorStateList colorStateList = new ColorStateList(states, colors);
-                tabView.setText(tabs[position]);
-                tabView.setTextSize(normalSize);
-                tabView.setTextColor(colorStateList);
+            int[] colors = new int[]{activeColor, normalColor};
+            ColorStateList colorStateList = new ColorStateList(states, colors);
+            tabView.setText(tabs[position]);
+            tabView.setTextSize(normalSize);
+            tabView.setTextColor(colorStateList);
 
-                tab.setCustomView(tabView);
-            }
+            tab.setCustomView(tabView);
         });
         //要执行这一句才是真正将两者绑定起来
         mediator.attach();
     }
 
-    private ViewPager2.OnPageChangeCallback changeCallback = new ViewPager2.OnPageChangeCallback() {
+    private final ViewPager2.OnPageChangeCallback changeCallback = new ViewPager2.OnPageChangeCallback() {
         @Override
         public void onPageSelected(int position) {
             //可以来设置选中时tab的大小
             int tabCount = tabLayout.getTabCount();
             for (int i = 0; i < tabCount; i++) {
                 TabLayout.Tab tab = tabLayout.getTabAt(i);
+                assert tab != null;
                 TextView tabView = (TextView) tab.getCustomView();
                 if (tab.getPosition() == position) {
+                    assert tabView != null;
                     tabView.setTextSize(activeSize);
                     tabView.setTypeface(Typeface.DEFAULT_BOLD);
                 } else {
+                    assert tabView != null;
                     tabView.setTextSize(normalSize);
                     tabView.setTypeface(Typeface.DEFAULT);
                 }
