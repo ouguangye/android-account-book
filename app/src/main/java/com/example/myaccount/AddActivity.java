a0fa4d4927bbc5bb3c7ce5c43b97a4eea4feef94
ou gunagye <3250679635@qq.com>
2021-12-01 20:00:25 +0800

    将AccountFrament设为AddActvity的内部类

--- app/src/main/java/com/example/myaccount/AddActivity.java
+++ app/src/main/java/com/example/myaccount/AddActivity.java
@@ -5,21 +5,28 @@ import static com.example.myaccount.DateUtils.FORMAT_M;
 import static com.example.myaccount.DateUtils.FORMAT_Y;
 import static com.example.myaccount.DateUtils.FORMAT_YMD;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -30,6 +37,7 @@ import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class AddActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -229,7 +237,7 @@ public class AddActivity extends AppCompatActivity implements View.OnClickListen
                 showPayinfoSelector();
                 break;
             case R.id.tb_note_date://日期
-                //showTimeSelector();
+                showTimeSelector();
                 break;
             case R.id.tb_note_remark://备注
                 //showContentDialog();
@@ -344,10 +352,126 @@ public class AddActivity extends AppCompatActivity implements View.OnClickListen
         }
     }
 
+    public void showTimeSelector() {
+        new DatePickerDialog(this, (DatePicker datePicker, int i, int i1, int i2) -> {
+            mYear = i;
+            mMonth = i1;
+            mDay = i2;
+            if (mMonth + 1 < 10) {
+                if (mDay < 10) {
+                    days = mYear + "-" + "0" +
+                            (mMonth + 1) + "-" + "0" + mDay;
+                } else {
+                    days = mYear + "-" + "0" +
+                            (mMonth + 1) + "-" + mDay;
+                }
+
+            } else {
+                if (mDay < 10) {
+                    days = mYear + "-" +
+                            (mMonth + 1) + "-" + "0" + mDay;
+                } else {
+                    days = mYear + "-" +
+                            (mMonth + 1) + "-" + mDay;
+                }
+
+            }
+            dateTv.setText(days);
+        }, mYear, mMonth, mDay).show();
+    }
+
     @Override
     protected void onDestroy() {
         mediator.detach();
         viewPager2.unregisterOnPageChangeCallback(changeCallback);
         super.onDestroy();
+    }
+
+    public static class AccountFragment extends Fragment {
+
+        private View rootView;
+        private List<Item> itemList = new ArrayList<>();
+
+
+        public static AccountFragment newInstance(int sign) {
+            Bundle args = new Bundle();
+            args.putInt("sign", sign);
+            AccountFragment fragment = new AccountFragment();
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            rootView = inflater.inflate(R.layout.fragment_account, container, false);
+
+            return rootView;
+        }
+
+        @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            initItems();
+            RecyclerView recyclerView = getActivity().findViewById(R.id.list_view);
+            GridLayoutManager gll = new GridLayoutManager(getActivity(), 6);
+            recyclerView.setLayoutManager(gll);
+            ItemAdapter itemAdapter = new ItemAdapter(itemList);
+            recyclerView.setAdapter(itemAdapter);
+        }
+
+
+        private void initItems(){
+    //        Item fork = new Item("三餐","fork",R.drawable.fork);
+    //        itemList.add(fork);
+    //
+    //        Item snack = new Item("零食","snack",R.drawable.snacks);
+    //        itemList.add(snack);
+    //
+    //        Item cloth = new Item("衣服","cloth",R.drawable.clothing);
+    //        itemList.add(cloth);
+    //
+    //        Item study = new Item("学习","study",R.drawable.study);
+    //        itemList.add(study);
+    //
+    //        Item traffic = new Item("交通","traffic",R.drawable.traffic);
+    //        itemList.add(traffic);
+
+            Item yiban = new Item("一般","yiban",R.mipmap.type_zijinbuchang);
+            itemList.add(yiban);
+
+            Item gouwu = new Item("购物","gouwu",R.mipmap.type_shangchengxiaofei);
+            itemList.add(gouwu);
+
+            Item yundong = new Item("运动","yundong",R.mipmap.type_yundong);
+            itemList.add(yundong);
+
+            Item yiliao = new Item("医疗","yiliao",R.mipmap.type_yiliao);
+            itemList.add(yiliao);
+
+            Item xuexi = new Item("学习","xuexi",R.mipmap.type_xuexi);
+            itemList.add(xuexi);
+
+            Item zhufang = new Item("住宿","zhufang",R.mipmap.type_zhufang);
+            itemList.add(zhufang);
+
+            Item weixiu = new Item("维修","weixiu",R.mipmap.type_weixiu);
+            itemList.add(weixiu);
+
+            Item yule = new Item("娱乐","yule",R.mipmap.type_yule);
+            itemList.add(yule);
+
+            Item tongxun = new Item("通讯","tongxun",R.mipmap.type_tongxun);
+            itemList.add(tongxun);
+
+            Item shuiguo = new Item("水果","shuiguo",R.mipmap.type_shuiguo);
+            itemList.add(shuiguo);
+
+            Item shuma = new Item("数码","shuma",R.mipmap.type_shuma);
+            itemList.add(shuma);
+
+            Item meirong = new Item("美容","meirong",R.mipmap.type_meirong);
+
+        }
     }
 }
--- app/src/main/java/com/example/myaccount/ItemAdapter.java
+++ app/src/main/java/com/example/myaccount/ItemAdapter.java
@@ -1,5 +1,6 @@
 package com.example.myaccount;
 
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,13 +14,17 @@ import java.util.List;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
     private List<Item>itemList;
+    private OnItemClickListener listener;
 
     static class ViewHolder extends RecyclerView.ViewHolder{
         ImageView itemImage;
         TextView typeName;
+        View itemView;
+        int selectedPosition=0;
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
+        public ViewHolder(@NonNull View view) {
+            super(view);
+            itemView = view;
             itemImage = itemView.findViewById(R.id.account_icon);
             typeName = itemView.findViewById(R.id.type_name);
         }
@@ -34,18 +39,41 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item,parent,false);
         ViewHolder holder = new ViewHolder(view);
+        holder.itemView.setOnClickListener(v -> {
+            int position = holder.getAdapterPosition();
+            Item item = itemList.get(position);
+            //Item preItem = itemList.get(holder.selectedPosition);
+           // holder.selectedPosition = position;
+
+        });
         return holder;
     }
 
+
+
+    @SuppressLint("ResourceAsColor")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Item item = itemList.get(position);
         holder.itemImage.setImageResource(item.getId());
         holder.typeName.setText(item.getName());
+        if(position== holder.selectedPosition){
+           // holder.itemImage.setBackgroundColor(R.color.blue_200);
+           // holder.itemImage.setBackgroundResource(R.drawable.blue_circle);
+        }
     }
+
+    public void setOnItemClickListener(OnItemClickListener listenser) {
+        this.listener = listenser;
+    }
+
 
     @Override
     public int getItemCount() {
         return itemList.size();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
     }
 }
