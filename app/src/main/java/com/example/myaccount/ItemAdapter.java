1213258351a33503008c21909dd7616bc396842d
ou gunagye <3250679635@qq.com>
2021-12-01 21:02:57 +0800

    取消了内部类（主要没啥用），新增接口，实现了类型的点击事件

--- app/src/main/java/com/example/myaccount/ItemAdapter.java
+++ app/src/main/java/com/example/myaccount/ItemAdapter.java
@@ -1,6 +1,7 @@
 package com.example.myaccount;
 
 import android.annotation.SuppressLint;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,7 @@ import java.util.List;
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
     private List<Item>itemList;
     private OnItemClickListener listener;
+
 
     static class ViewHolder extends RecyclerView.ViewHolder{
         ImageView itemImage;
@@ -39,13 +41,6 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item,parent,false);
         ViewHolder holder = new ViewHolder(view);
-        holder.itemView.setOnClickListener(v -> {
-            int position = holder.getAdapterPosition();
-            Item item = itemList.get(position);
-            //Item preItem = itemList.get(holder.selectedPosition);
-           // holder.selectedPosition = position;
-
-        });
         return holder;
     }
 
@@ -57,10 +52,12 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
         Item item = itemList.get(position);
         holder.itemImage.setImageResource(item.getId());
         holder.typeName.setText(item.getName());
-        if(position== holder.selectedPosition){
-           // holder.itemImage.setBackgroundColor(R.color.blue_200);
-           // holder.itemImage.setBackgroundResource(R.drawable.blue_circle);
-        }
+        holder.itemView.setOnClickListener(view -> {
+            if(listener!=null){
+                Log.e("click", "!");
+                listener.onItemClick(position);
+            }
+        });
     }
 
     public void setOnItemClickListener(OnItemClickListener listenser) {
